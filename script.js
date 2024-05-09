4ef8c7d28040e6332bcf62b556b89b95ebdb4088
guicoding <163929632+guicodeworld@users.noreply.github.com>
2024-05-09 18:58:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburguer-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e"
+];
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+  circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+var phrase = document.getElementById('phrase')
+var shadow = '';
+for(var i = 0; i < 500; i++)
+{
+    shadow +=(shadow? ',':'')+ i*1+'px '+ i*1+'px 0 rgb(85,85,85)';
+}
+phrase.style.textShadow = shadow;
+
+document.getElementById("home-button").addEventListener("click", function() {
+  window.location.href = "https://www.youtube.com/";
+});
+
+function downloadFile() {
+  
+  var downloadLink = document.createElement('a');
+  
+  downloadLink.href = 'CV Guilherme Souza.pdf'; 
+  
+  downloadLink.download = 'CV Guilherme Souza'; 
+  
+  document.body.appendChild(downloadLink);
+  
+  downloadLink.click();
+  
+  document.body.removeChild(downloadLink);
+}
